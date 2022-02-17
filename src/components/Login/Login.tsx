ff55899c9e4c0a8d94b829b525a9e25811746ce5
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-02-17 01:15:53 +0100

    create login form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,22 @@
 import * as React from "react";
+import './Login.scss';
 
 type LoginProps = {
   //
 };
 
 const Login: React.FC<LoginProps> = () => {
-  return <div>Login</div>;
+  return <form>
+          <div>
+            <label htmlFor="user">Username: </label>
+            <input type="text" name="user"/>
+          </div>
+          <div>
+            <label htmlFor="pass">Password: </label>
+            <input type="text" name="password"/>
+          </div>  
+          <input type="submit" value='Search'/>  
+         </form>;
 };
 
 export default Login;
